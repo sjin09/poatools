611a48cae6cc079cf1a850e922c1a6f3cbf36bec
Sangjin <sl17@sanger.ac.uk>
2021-08-31 14:40:34 +0100

    poatools: poa and filter

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-## poatools poa --cell m64125_201017_124255 --zmw 60426096 --ccs ./m64125_201017_124255/ccs/60426096.fastq --subreads ./m64125_201017_124255/subreads/60426096.fasta --qsub 60426096:7_T/G --length 100 --poa 60426096.poa --poa_subset 60426096.subset.poa --pdf
-
-
